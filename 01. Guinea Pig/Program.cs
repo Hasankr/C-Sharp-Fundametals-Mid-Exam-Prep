38fb8137247596b87e4bee9ab33a26d1600e9cc8
hasankr <hasankr@abv.bg>
2022-06-24 23:07:14 +0300

    Add project files.

---
+++ 01. Guinea Pig/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._Guinea_Pig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
