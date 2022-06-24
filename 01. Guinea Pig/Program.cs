106da4d03ce80597f5ae344e89a47aaa82ed0a61
hasankr <hasankr@abv.bg>
2022-06-24 23:58:37 +0300

    added 1.0

--- 01. Guinea Pig/Program.cs
+++ 01. Guinea Pig/Program.cs
@@ -6,7 +6,31 @@ namespace _01._Guinea_Pig
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double totalFood = double.Parse(Console.ReadLine()) * 1000;
+            double totalHay = double.Parse(Console.ReadLine()) * 1000;
+            double totalCover = double.Parse(Console.ReadLine()) * 1000;
+            double pigWeight = double.Parse(Console.ReadLine()) * 1000;
+            for (int i = 1; i <= 30; i++)
+            {
+                totalFood -= 300;
+                if (i % 2 == 0)
+                {
+                    totalHay -= 0.05 * totalFood;
+                }
+                if (i % 3 == 0)
+                {
+                    totalCover -= pigWeight / 3;
+                }
+                
+            }
+            if (totalFood <= 0 || totalHay <= 0 || totalCover <= 0)
+            {
+                Console.WriteLine("Merry must go to the pet store!");
+            }
+            else
+            {
+                Console.WriteLine($"Everything is fine! Puppy is happy! Food: {totalFood / 1000:f2}, Hay: {totalHay/ 1000:f2}, Cover: {totalCover / 1000:f2}.");
+            }
         }
     }
 }
